e724ca0537fa574287e71b91d21629a51e0bca60
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-02-05 22:51:29 +0530

    Update ldap_bruteforce.py

--- files/ldap_bruteforce.py
+++ files/ldap_bruteforce.py
@@ -36,7 +36,7 @@ def display_banner():
     """Displays the banner for the script."""
     colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
     banner_color = choice(colors)
-    banner = pyfiglet.figlet_format("Kraken LDAP Brute-Force", font="slant")
+    banner = pyfiglet.figlet_format("Shivay LDAP Brute-Force", font="slant")
     print(Style.BRIGHT + banner_color + banner)
     print(Style.RESET_ALL + Fore.WHITE + "="*80 + "\n")
 
